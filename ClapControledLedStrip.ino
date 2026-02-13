58afae2e0120d024b0e0c64b8b1e64647b0ddf10
KostantinRudenko <rudenkokostya20@gmail.com>
2026-02-14 00:01:13 +0200

    create a separate function for getting amplitude

--- ClapControledLedStrip.ino
+++ ClapControledLedStrip.ino
@@ -29,32 +29,25 @@ void setup() {
 void loop() {
   switch (st) {
     case ClapChecking:
-      minV = 1023;
-      maxV = 0;
-      start = millis();
-      while (millis() - start < 50) {
-        value = analogRead(MPIN);
-        if (minV > value) minV = value;
-        if (maxV < value) maxV = value;
 
-        uint16_t A = maxV - minV;
+      static uint16_t A;
+      GetAmp(A);
 
-        if (!isClapDetected && A > Limit) {
-          clapsAmount++;
-          isClapDetected = true;
-          clapWaitingStartTime = millis();
+      if (!isClapDetected && A > Limit) {
+        clapsAmount++;
+        isClapDetected = true;
+        clapWaitingStartTime = millis();
 
-          leds[clapsAmount-1] = CRGB(0,0,10);
-          FastLED.show();
+        leds[clapsAmount-1] = CRGB(0,0,10);
+        FastLED.show();
 
-          Serial.print("Clap detected. New amount: ");
-          Serial.println(clapsAmount);
-        }
+        Serial.print("Clap detected. New amount: ");
+        Serial.println(clapsAmount);
+      }
 
-        if (isClapDetected && A < Limit) {
-          isClapDetected = false;
-          Serial.println("Amplitude is lower than limit");
-        }
+      if (isClapDetected && A < Limit) {
+        isClapDetected = false;
+        Serial.println("Amplitude is lower than limit");
       }
 
       if (millis() - clapWaitingStartTime > ClapWaitingTime && clapsAmount != 0) {
