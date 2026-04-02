f25611521f687cdf847657cc8acdda5560c8434a
KostantinRudenko <rudenkokostya20@gmail.com>
2026-04-02 20:50:48 +0300

    added mode parameter and setMode, executeMode function to the ledStrip class

--- ledStrip.h
+++ ledStrip.h
@@ -8,10 +8,13 @@ class LedStrip {
 	private:
 		uint8_t _ledPin;
 		uint16_t _ledsAmount;
+		uint8_t _currentMode;
 		CRGB _leds[LEDS_AMOUNT];
 	public:
 		LedStrip();
 		void clear();
+		void setMode(uint8_t mode);
+		bool executeMode();
 		bool fillFromColorToColor(const CRGB& FromColor, const CRGB& ToColor);
 		void alarmMode();
 		bool blueFillMode();
